744dd3ab0f6fd9f86ce002cb70d0da6e854e6a1c
Moustafa Mohamed <musta7188@hotmail.it>
2020-01-23 11:55:47 +0000

    Automatically backed up by Learn

--- spec/css_graffiti_override_spec.rb
+++ spec/css_graffiti_override_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe 'css/cleanup.css' do
   end
 
   it 'has overrides added' do
-    minimum_bytes = o
+    minimum_bytes = 0
     size = File.size("css/cleanup.css")
     expect(size).to be > minimum_bytes, "You should have at least 300 bytes worth of updates to css/cleanup.css"
   end
